421d9b0f1c121939bcde7473b9166fc0bc9621ca
Skillfun9 <mequinho03@gmail.com>
2021-03-08 18:19:35 +0000

    Server

---
+++ Backend/controllers/user.controller.js
@@ -0,0 +1,42 @@
+const connect = require('../config/connect.js')
+//const pool = require('db-pool');
+
+
+function read(req, res) {
+connect.getConnection(function (err, conn) {
+    if (err)
+        return res.send(400);
+
+    // if you got a connection...
+    conn.query('SELECT * FROM users ', function(err, rows) {
+        if(err) {
+            conn.release();
+
+
+        }
+
+        // for simplicity, just send the rows
+        res.send(rows);
+        // CLOSE THE CONNECTION
+        conn.release();
+    })
+});}
+
+
+
+
+
+
+
+
+
+
+
+
+//Export functions
+module.exports = {
+
+    read: read,
+
+
+};
--- Backend/routes/main.route.js
+++ Backend/routes/main.route.js
@@ -1,37 +1,31 @@
 const router =require('express').Router();
 
-/*
-const controllerBolt = require('../controllers/boltMoloni.controller.js');
-const controllerCompany = require('../controllers/company.controller.js');
-const controllerCategory = require('../controllers/category.controller.js');
-const controllerUber = require('../controllers/uberJasmin.controller.js');
-const controllerKapten = require('../controllers/kaptenPipeDrive.controller.js');
-const controllerPaypal = require('../controllers/paypalPayment.controller.js');
+
 const controllerUser = require('../controllers/user.controller.js');
 
 
 //Bolt using moloni
-router.get('/bolt/', controllerBolt.getPackages);
-router.get('/company/', controllerCompany.getCompany);
-router.get('/category/', controllerCategory.getCategory);
+//router.get('/bolt/', controllerBolt.getPackages);
+//router.get('/company/', controllerCompany.getCompany);
+//router.get('/category/', controllerCategory.getCategory);
 
 //Uber using Jasmin
-router.get('/uber/', controllerUber.getPacotes);
+//router.get('/uber/', controllerUber.getPacotes);
 
 //Kapten using Pipedrive
-router.get('/kapten/', controllerKapten.getPackages);
+//router.get('/kapten/', controllerKapten.getPackages);
 
 //Paypal payment
-router.get('/paypal/', controllerPaypal.getPay); //mudar o nome da funçao 'getPay' se for preciso
-router.get('/success/', controllerPaypal.getParameters);
-router.get('/cancel/', controllerPaypal.getCancel);
+//router.get('/paypal/', controllerPaypal.getPay); //mudar o nome da funçao 'getPay' se for preciso
+//router.get('/success/', controllerPaypal.getParameters);
+//router.get('/cancel/', controllerPaypal.getCancel);
 
 //Rota Users
-router.get('/users/', controllerUser.read);
-router.get('/cliques/', controllerUser.readC);
-router.post('/cliquesU/',controllerUser.saveU);
-router.post('/cliquesK/',controllerUser.saveK);
-router.post('/cliquesB/',controllerUser.saveB);
+//router.get('/users/', controllerUser.read);
+//router.get('/cliques/', controllerUser.readC);
+//router.post('/cliquesU/',controllerUser.saveU);
+//router.post('/cliquesK/',controllerUser.saveK);
+//router.post('/cliquesB/',controllerUser.saveB);
 
 
 
