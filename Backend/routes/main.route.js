5aaede3037991c78c347b6a8d3111e6f6484bfdc
Skillfun9 <mequinho03@gmail.com>
2021-03-08 15:35:19 +0000

    Server

---
+++ Backend/routes/main.route.js
@@ -0,0 +1,38 @@
+const router =require('express').Router();
+
+/*
+const controllerBolt = require('../controllers/boltMoloni.controller.js');
+const controllerCompany = require('../controllers/company.controller.js');
+const controllerCategory = require('../controllers/category.controller.js');
+const controllerUber = require('../controllers/uberJasmin.controller.js');
+const controllerKapten = require('../controllers/kaptenPipeDrive.controller.js');
+const controllerPaypal = require('../controllers/paypalPayment.controller.js');
+const controllerUser = require('../controllers/user.controller.js');
+
+
+//Bolt using moloni
+router.get('/bolt/', controllerBolt.getPackages);
+router.get('/company/', controllerCompany.getCompany);
+router.get('/category/', controllerCategory.getCategory);
+
+//Uber using Jasmin
+router.get('/uber/', controllerUber.getPacotes);
+
+//Kapten using Pipedrive
+router.get('/kapten/', controllerKapten.getPackages);
+
+//Paypal payment
+router.get('/paypal/', controllerPaypal.getPay); //mudar o nome da funçao 'getPay' se for preciso
+router.get('/success/', controllerPaypal.getParameters);
+router.get('/cancel/', controllerPaypal.getCancel);
+
+//Rota Users
+router.get('/users/', controllerUser.read);
+router.get('/cliques/', controllerUser.readC);
+router.post('/cliquesU/',controllerUser.saveU);
+router.post('/cliquesK/',controllerUser.saveK);
+router.post('/cliquesB/',controllerUser.saveB);
+
+
+
+module.exports = router;
---
+++ Backend/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || '127.0.0.1';
+
+
+
+//carregar bibliotecas globais
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const expressSanitizer = require('express-sanitizer');
+const expressValidator = require('express-validator');
+
+//iniciar a aplicação
+var app = express();
+
+app.use(bodyParser.json({ type: 'application/json' }), bodyParser.urlencoded({ extended: true }));
+app.use(expressSanitizer());
+app.use(expressValidator());
+
+// servir front-end
+app.use(express.static('../conexi-html-files'))
+app.use(express.static('../Painel'))
+
+app.listen(port, function (err) {
+    if (!err) {
+        console.log('Your app is listening on ' + host + ' and port ' + port);
+    }
+    else { console.log(err); }
+});
+
+//forçar utilização das bibliotecas
+app.use(cors());
+
+//colocar aqui código para express-sessions
+
+module.exports = app;
+
+
+require('./loader.js');
+
