dc9085e155d9b6f4fcc1f6fab2f8c1e1cd405817
Skillfun9 <mequinho03@gmail.com>
2021-04-12 23:05:34 +0100

    search controller

--- Backend/routes/main.route.js
+++ Backend/routes/main.route.js
@@ -2,7 +2,7 @@ const router =require('express').Router();
 
 
 const controllerUser = require('../controllers/user.controller.js');
-const controlleSearch = require('../controllers/search.controller.js');
+const controllerSearch = require('../controllers/search.controller.js');
 
 
 module.exports = router;
