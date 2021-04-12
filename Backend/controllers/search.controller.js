bd0d9a3354e570c530e610fc5bb509bcd2bf2819
antonipt4 <antoniopt4@gmail.com>
2021-04-12 23:14:48 +0100

    test py

--- Backend/controllers/search.controller.js
+++ Backend/controllers/search.controller.js
@@ -1,6 +1,6 @@
 const spawn = require('child_process').spawn;
 
-const process = spawn('python',['/script.py']);
+const process = spawn('python',['/my.py']);
 
 process.stdout.on('data', data => {
     console.log(data.toString());
