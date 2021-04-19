33f9815bb6cfa9bea510fbfdefd268f3167ed2a1
Luis Paula <luispaula@Air-de-Luis-2.Home>
2021-04-19 15:09:34 +0100

    ddsd

---
+++ Frontend/coreui-free-vue-admin-template-master/src/containers/TheFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <CFooter :fixed="false">
+    <div>
+      <a href="https://coreui.io" target="_blank"></a>
+      <span class="ml-1"></span>
+    </div>
+    <div class="mfs-auto">
+      <span class="mr-1" target="_blank"></span>
+      
+    </div>
+  </CFooter>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter'
+}
+</script>
---
+++ Frontend/coreui-free-vue-admin-template-master/src/views/users/UsersData.js
@@ -0,0 +1,31 @@
+const usersData = [
+  { Autor: 'Samppa Nori', Data: '2012/01/01',  pesquisa: 'Machine learning'},
+  { Autor: 'Estavan Lykos', Data: '2012/02/01',  pesquisa: 'Database'},
+  { Autor: 'Chetan Mohamed', Data: '2012/02/01',  pesquisa: 'python'},
+  { Autor: 'Derick Maximinus', Data: '2012/03/01',  pesquisa: 'Machine learning'},
+  { Autor: 'Friderik Dávid', Data: '2012/01/21',  pesquisa: 'Database'},
+  { Autor: 'Yiorgos Avraamu', Data: '2012/01/01',  pesquisa: 'Machine learning'},
+  { Autor: 'Avram Tarasios', Data: '2012/02/01',  pesquisa: 'Database', _classes: 'table-success'},
+  { Autor: 'Quintin Ed', Data: '2012/02/01',  pesquisa: 'python'},
+  { Autor: 'Enéas Kwadwo', Data: '2012/03/01',  pesquisa: 'Machine learning'},
+  { Autor: 'Agapetus Tadeáš', Data: '2012/01/21',  pesquisa: 'Database'},
+  { Autor: 'Carwyn Fachtna', Data: '2012/01/01',  pesquisa: 'Machine learning', _classes: 'table-success'},
+  { Autor: 'Nehemiah Tatius', Data: '2012/02/01',  pesquisa: 'Database'},
+  { Autor: 'Ebbe Gemariah', Data: '2012/02/01',  pesquisa: 'python'},
+  { Autor: 'Eustorgios Amulius', Data: '2012/03/01',  pesquisa: 'Machine learning'},
+  { Autor: 'Leopold Gáspár', Data: '2012/01/21',  pesquisa: 'Database'},
+  { Autor: 'Pompeius René', Data: '2012/01/01',  pesquisa: 'Machine learning'},
+  { Autor: 'Paĉjo Jadon', Data: '2012/02/01',  pesquisa: 'Database'},
+  { Autor: 'Micheal Mercurius', Data: '2012/02/01',  pesquisa: 'python'},
+  { Autor: 'Ganesha Dubhghall', Data: '2012/03/01',  pesquisa: 'Machine learning'},
+  { Autor: 'Hiroto Šimun', Data: '2012/01/21',  pesquisa: 'Database'},
+  { Autor: 'Vishnu Serghei', Data: '2012/01/01',  pesquisa: 'Machine learning'},
+  { Autor: 'Zbyněk Phoibos', Data: '2012/02/01',  pesquisa: 'Database'},
+  { Autor: 'Einar Randall', Data: '2012/02/01',  pesquisa: 'python'},
+  { Autor: 'Félix Troels', Data: '2012/03/21',  pesquisa: 'Database'},
+  { Autor: 'Aulus Agmundr', Data: '2012/01/01',  pesquisa: 'Machine learning' },
+]
+
+export default usersData
+
+
