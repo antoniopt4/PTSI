88972b67e9c2b29f32fe382787c92a834ebafca8
Skillfun9 <mequinho03@gmail.com>
2021-04-28 14:00:48 +0100

    controller and searchpage updated

--- Backend/controllers/search.controller.js
+++ Backend/controllers/search.controller.js
@@ -5,7 +5,7 @@ async function getFlaskd(req,res) {
     var data = { 
         
     
-        nome: req.body.nome   //dado que vem do form do frontend
+        topico: req.body.nome   //dado que vem do form do frontend
     } 
 
     var options = { 
