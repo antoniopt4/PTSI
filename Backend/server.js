22d5a97f980fd88497b6611e3bbf906fd083a758
Skillfun9 <mequinho03@gmail.com>
2021-05-21 00:47:21 +0100

    MongoDB updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || '127.0.0.1';
+const { MongoClient } = require('mongodb');
 
 
 
@@ -40,3 +41,49 @@ module.exports = app;
 
 require('./loader.js');
 
+/*----------------------------------------------------START MONGODB---------------------------------------------------------*/
+
+async function main() {
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://apt2:yM0lRvFoklUsYgz2@cluster0.0zcqp.mongodb.net/gs?retryWrites=true&w=majority";
+
+    /**
+     * The Mongo Client you will use to interact with your database
+     * See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html for more details
+     */
+     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        
+        await findProfiles(client);
+        
+
+    
+    } finally {
+        // Close the connection to the MongoDB cluster
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+// Add functions that make DB calls here
+
+//Read Function
+async function findProfiles(client) {
+  
+    const result = await client.db("gs").collection("Profiles").find().toArray();
+
+      if (result) {
+        
+        console.log(result);
+    } else {
+        console.log(`No results found!`);
+    }
+}
